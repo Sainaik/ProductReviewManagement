4d7e32c67ed14ca3044abfe6efb6e76a2bfc3ace
Sai Kumar Naik <saikumar.naik7777@gmail.com>
2020-11-06 15:32:56 +0530

    [Sai]Added. Product Review Management system

---
+++ ProductReviewManagement/Management.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+
+namespace ProductReviewManagement
+{
+    class Management
+    {
+        public readonly DataTable dataTable = new DataTable();
+
+        //UC2
+        public void TopRecords(List<ProductReview> productReviewsList)
+        {
+            var recordedData = (from productReview in productReviewsList
+                                orderby productReview.Rating descending
+                                select productReview).Take(3);
+
+
+            Console.WriteLine("Top Reviews");
+
+            foreach (var list in recordedData)
+            {
+                Console.WriteLine($"ProductId: {list.ProductId}, UserId : {list.UserId}, Rating: {list.Rating}, Review : {list.Review}, IsLike : {list.IsLike}");
+            }
+
+        }
+
+        // UC3
+        public void SelectRecords(List<ProductReview> productReviewsList)
+        {
+            var recordedData = (from productReview in productReviewsList
+                                where ((productReview.ProductId == 1 && productReview.Rating > 3) || (productReview.ProductId == 4 && productReview.Rating > 3) || (productReview.ProductId == 9 && productReview.Rating > 3))
+                                select productReview);
+
+
+            Console.WriteLine("Records with review greater than 3 in (1,4,9)");
+
+            foreach (var list in recordedData)
+            {
+                Console.WriteLine($"ProductId: {list.ProductId}, UserId : {list.UserId}, Rating: {list.Rating}, Review : {list.Review}, IsLike : {list.IsLike}");
+            }
+
+        }
+
+
+        // UC4
+        public void CountOfReviews(List<ProductReview> productReviewsList)
+        {
+            var recordedData = productReviewsList.GroupBy(x => x.ProductId).Select(x => new { ProductId = x.Key, Count = x.Count() });
+
+            Console.WriteLine("Records Id's with Review count ");
+
+            foreach (var list in recordedData)
+            {
+                Console.WriteLine($"ProductId: {list.ProductId}, Count : {list.Count}"); 
+            }
+
+        }
+
+
+        //UC5 & 7
+
+        public void ProductIdwithReviews(List<ProductReview> productReviewsList)
+        {
+            var recordedData = (from productReview in productReviewsList                     
+                                select new { productId = productReview.ProductId, Review = productReview.Review } );
+            Console.WriteLine("Records Id's with Review");
+
+            foreach (var list in recordedData)
+            {
+                Console.WriteLine($"ProductId: {list.productId}, Review: {list.Review}");
+            }
+
+        }
+
+        // UC6
+
+        public void SkipTop5Records(List<ProductReview> productReviews)
+        {
+            var records = (from productReview in productReviews select productReview).Skip(5);
+
+            Console.WriteLine("Records after skipping first 5");
+
+            foreach (var list in records)
+            {
+                Console.WriteLine($"ProductId: {list.ProductId}, UserId : {list.UserId}, Rating: {list.Rating}, Review : {list.Review}, IsLike : {list.IsLike}");
+            }
+
+
+        }
+
+        // UC 9
+        public void LikedProducts(List<ProductReview> productReviewsList)
+        {
+            var records = from product in productReviewsList
+                          where product.IsLike == true
+                          select product;
+
+            Console.WriteLine("Liked Products are");
+
+            foreach (var list in records)
+            {
+                Console.WriteLine($"ProductId: {list.ProductId}, UserId : {list.UserId}, Rating: {list.Rating}, Review : {list.Review}, IsLike : {list.IsLike}");
+            }
+
+        }
+
+        // UC10 Proucts with their Avarage Rating
+        public void ProductAvarageRating(List<ProductReview> productReviews)
+        {
+            var records = productReviews.GroupBy(x => x.ProductId).Select(x => new { ProductId = x.Key, Average = x.Average() });
+
+            Console.WriteLine("Liked Products are");
+
+            foreach (var list in records)
+            {
+                Console.WriteLine($"ProductId: {list.ProductId}, Count : {list.Average}");
+            }
+        }
+
+        }
+
+    }
+}
---
+++ ProductReviewManagement/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ProductReviewManagement
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Product Review problem");
+
+            List<ProductReview> productReviewsList = new List<ProductReview>()
+            {
+
+                new ProductReview(){ProductId = 1, UserId = 1, Rating = 5, Review = "cool", IsLike = true},
+
+                new ProductReview(){ProductId = 2, UserId = 9, Rating = 4, Review = "Not Bad", IsLike = true},
+
+                new ProductReview(){ProductId = 3, UserId = 2, Rating = 4, Review = "good", IsLike = true },
+
+                new ProductReview(){ProductId = 4, UserId = 3, Rating = 3, Review = "avarage", IsLike = true},
+
+                new ProductReview(){ProductId = 5, UserId = 4, Rating = 2, Review = "bad", IsLike = false },
+
+                new ProductReview(){ProductId = 6, UserId = 5, Rating = 5, Review = "cool", IsLike = true},
+
+                new ProductReview(){ProductId = 7, UserId = 6, Rating = 4, Review = "good", IsLike = true },
+
+                new ProductReview(){ProductId = 8, UserId = 7, Rating = 3, Review = "avarage", IsLike = true},
+
+                new ProductReview(){ProductId = 9, UserId = 8, Rating = 2, Review = "bad", IsLike = false },
+
+                new ProductReview(){ProductId = 10, UserId = 3, Rating = 1, Review = "bad", IsLike = false },
+            };
+
+            foreach(var list in productReviewsList)
+            {
+                Console.WriteLine($"ProductId: {list.ProductId}, UserId : {list.UserId}, Rating: {list.Rating}, Review : {list.Review}, IsLike : {list.IsLike}");
+            }
+
+
+            Management management = new Management();
+            //management.TopRecords(productReviewsList);
+            //management.SelectRecords(productReviewsList);
+            //management.CountOfReviews(productReviewsList);
+            //management.ProductIdwithReviews(productReviewsList);
+            //management.SkipTop5Records(productReviewsList);
+            //management.LikedProducts(productReviewsList);
+            management.ProductAvarageRating(productReviewsList);
+        }
+    }
+}
